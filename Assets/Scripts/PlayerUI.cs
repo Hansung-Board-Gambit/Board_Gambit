2836b2fd8006448a454e21289915290a9cc415ec
Jinsoo1210 <pjsoo1210@gmail.com>
2026-05-29 00:04:04 +0900

    match UI added
    승패 UI 추가

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -1,23 +1,31 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
     public static PlayerUI instance;
 
     [Header("UI ¿¬°á")]
-    [SerializeField] TextMeshProUGUI hpText;
-    [SerializeField] TextMeshProUGUI ammoText;
+    [SerializeField] private Image hpFillImage;
+    [SerializeField] private TextMeshProUGUI ammoText;
+    [SerializeField] private TextMeshProUGUI hostNameText;
+    [SerializeField] private TextMeshProUGUI guestNameText;
     //[SerializeField] TextMeshProUGUI grapplingText;
 
     private void Awake()
     {
-        if(instance == null) instance = this;
-        else Destroy(gameObject);
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject); // ÇÙ½É
     }
+
     public void Configure(TextMeshProUGUI hp, TextMeshProUGUI ammo)
     {
-        hpText = hp;
         ammoText = ammo;
         instance = this;
         Clear();
@@ -25,21 +33,30 @@ public class PlayerUI : MonoBehaviour
 
     public void Clear()
     {
-        if (hpText != null)
-            hpText.text = "HP : ";
-
         if (ammoText != null)
             ammoText.text = "Ammo : ";
     }
 
-    public void UpdateHPText(int currentHP, int maxHP)
+    public void UpdateHP(int currentHP, int maxHP)
     {
-        if (hpText != null) hpText.text = $"HP : {currentHP}/{maxHP}";
+        Debug.Log($"HP UI Update: {currentHP}/{maxHP}");
+        if (hpFillImage == null) return;
+        float ratio = (float)currentHP / maxHP;
+        hpFillImage.fillAmount = (float)currentHP / maxHP;
     }
 
     public void UpdateAmmoText(int currentAmmo, int maxAmmo)
     {
         if (ammoText != null) ammoText.text = $"Ammo : {currentAmmo}/{maxAmmo}";
+    }
+
+    public void UpdateNames(string hostName, string guestName)
+    {
+        if (hostNameText != null)
+            hostNameText.text = hostName;
+
+        if (guestNameText != null)
+            guestNameText.text = guestName;
     }
 
     public void UpdateCurrentGrapplingText(int currentGrappling, int maxGrappling)
