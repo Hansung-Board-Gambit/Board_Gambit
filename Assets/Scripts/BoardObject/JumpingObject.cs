21aea23726d70f57d2f6305b90f3b3e7758d28d8
Jinsoo1210 <pjsoo1210@gmail.com>
2026-06-02 13:36:09 +0900

    UI, sound fix
    1. 패배시 체력 남아있던 버그 수정
    2. 카운트 다운 효과음 추가
    3. 종료 임박 효과음 추가

--- Assets/Scripts/BoardObject/JumpingObject.cs
+++ Assets/Scripts/BoardObject/JumpingObject.cs
@@ -6,6 +6,15 @@ public class JumpingObject : NetworkBehaviour
     [Header("점프힘 설정")]
     [SerializeField] float jumpForce = 20f;
 
+    [Header("사운드")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip jumpPadSfx;
+
+    public override void Spawned()
+    {
+        Debug.Log("JumpPad Spawned!");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Player player = other.GetComponent<Player>();
@@ -13,6 +22,14 @@ public class JumpingObject : NetworkBehaviour
         if(player != null )
         {
             player.ApplyJumpPadForce(jumpForce);
+            
+            RPC_PlayJumpPadSfx();
         }
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void RPC_PlayJumpPadSfx()
+    {
+        audioSource.PlayOneShot(jumpPadSfx);
     }
 }
